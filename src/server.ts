23bd9c0438654dee9c9264ca31dc504f2494a0d3
Angelo Munhoz <angelomca09@gmail.com>
2023-05-26 10:31:57 -0300

    Upload Route

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,21 @@ import "dotenv/config";
 import fastify from "fastify";
 import cors from "@fastify/cors";
 import jwt from "@fastify/jwt";
+import multipart from "@fastify/multipart";
 import { memoriesRoutes } from "./routes/memories";
 import { authRoutes } from "./routes/auth";
+import { uploadRoutes } from "./routes/upload";
+import { resolve } from "node:path";
 
 const app = fastify();
+
+app.register(multipart);
+
+// outra maneira de importar pacotes
+app.register(require("@fastify/static"), {
+  root: resolve(__dirname, "../uploads"),
+  prefix: "/uploads",
+});
 
 app.register(cors, {
   origin: true, // todas URLs de frontend poderão acessar nosso backend
@@ -16,6 +27,7 @@ app.register(jwt, {
   secret: "spacetime", // Chave que diferencia token desta aplicação das demais
 });
 
+app.register(uploadRoutes);
 app.register(authRoutes);
 app.register(memoriesRoutes);
 
